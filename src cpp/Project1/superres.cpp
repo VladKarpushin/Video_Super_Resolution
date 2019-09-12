a4db3c42e02ffd749ceaf1775d49ccdb4909088e
Vlad Karpushin <karpushin@ngs.ru>
2019-09-12 15:19:11 +0700

    code: +++

--- src cpp/Project1/superres.cpp
+++ src cpp/Project1/superres.cpp
@@ -26,7 +26,8 @@ int FindOffset(const Mat & inputImg, const Mat & inputImgTemplate, Point & maxLo
 int main() 
 {
  	//VideoCapture cap("D:\\home\\programming\\vc\\new\\6_My home projects\\11_video processing\\input\\screen1.avi");
-	VideoCapture cap("D:\\home\\programming\\vc\\new\\6_My home projects\\11_video processing\\input\\!moon_zoom_2.MOV");
+	//VideoCapture cap("D:\\home\\programming\\vc\\new\\6_My home projects\\11_video processing\\input\\!moon_zoom_2.MOV");
+	VideoCapture cap("D:\\home\\programming\\vc\\new\\6_My home projects\\11_video processing\\input\\plane3.MOV");
 	String srtOutPath = "D:\\home\\programming\\vc\\new\\6_My home projects\\11_video processing\\output\\";
 
 	if (!cap.isOpened())
@@ -44,10 +45,25 @@ int main()
 	}
 	cvtColor(frameCam, frameCam, COLOR_BGR2GRAY);
 
-	const Rect roiRef = Rect(Point2i(800, 350), Point2i(1300, 800));		// for !moon_zoom_2.MOV
-	const Rect roiTemplate = Rect(Point2i(972, 475), Point2i(1075, 586));	// for !moon_zoom_2.MOV
-	const int SCALE_FACTOR = 5; // for screen1.avi
-	const int MAX_OBJ_OFFSET = 100 * SCALE_FACTOR; // max allowed radius of object offset MAX_OBJ_OFFSET = 100 for screen1.avi
+	const Rect roiRef = Rect(Point2i(820, 312), Point2i(1200, 640));		// for plane3.MOV
+	const Rect roiTemplate = Rect(Point2i(954, 445), Point2i(1043, 498));	// for plane3.MOV
+	const int SCALE_FACTOR = 5; // for plane3.MOV
+	const int MAX_OBJ_OFFSET = 300 * SCALE_FACTOR; // max allowed radius of object offset MAX_OBJ_OFFSET = 100 for screen1.avi
+
+	//const Rect roiRef = Rect(Point2i(878, 435), Point2i(1225, 753));		// for plane1.MOV
+	//const Rect roiTemplate = Rect(Point2i(942, 524), Point2i(1092, 619));	// for plane1.MOV
+	//const int SCALE_FACTOR = 5; // for plane1.MOV
+	//const int MAX_OBJ_OFFSET = 200 * SCALE_FACTOR; // max allowed radius of object offset MAX_OBJ_OFFSET = 100 for screen1.avi
+
+	//const Rect roiRef = Rect(Point2i(845, 304), Point2i(1300, 663));		// for plane2.MOV
+	//const Rect roiTemplate = Rect(Point2i(955, 415), Point2i(1115, 528));	// for plane2.MOV
+	//const int SCALE_FACTOR = 5; // for plane2.MOV
+	//const int MAX_OBJ_OFFSET = 200 * SCALE_FACTOR; // max allowed radius of object offset MAX_OBJ_OFFSET = 100 for screen1.avi
+												   
+	//const Rect roiRef = Rect(Point2i(800, 350), Point2i(1300, 800));		// for !moon_zoom_2.MOV
+	//const Rect roiTemplate = Rect(Point2i(972, 475), Point2i(1075, 586));	// for !moon_zoom_2.MOV
+	//const int SCALE_FACTOR = 5; // for !moon_zoom_2.MOV
+	//const int MAX_OBJ_OFFSET = 100 * SCALE_FACTOR; // max allowed radius of object offset MAX_OBJ_OFFSET = 100 for screen1.avi
 
 	//const Rect roiRef = Rect(Point2i(117, 525), Point2i(662, 871));		// for screen1.avi
 	//const Rect roiTemplate = Rect(Point2i(301, 621), Point2i(407, 731));	// for screen1.avi
@@ -61,14 +77,14 @@ int main()
 	
 	Point offset(roiTemplate.x - roiRef.x, roiTemplate.y - roiRef.y);
 	offset *= SCALE_FACTOR;
-	const Rect roiRefTemplate = Rect(offset, roiTemplate.size() * SCALE_FACTOR);
-
-	Mat img_averaged = Mat(roiTemplate.size() * SCALE_FACTOR, CV_32F, Scalar(0));	// superresolution image
+	Size2i size_out = (roiTemplate.size() - Size2i(1, 1)) * SCALE_FACTOR;
+	const Rect roiRefTemplate = Rect(offset, size_out);
+	Mat img_averaged = Mat(size_out, CV_32F, Scalar(0));	// superresolution image
 	//Mat imgAvgB = Mat(roiTemplate.size() * SCALE_FACTOR, CV_32F, Scalar(0));
 	int i = 0;
 	int iNumAveragedFrames = 0;
 	//while (1)
-	while (iNumAveragedFrames < 10)
+	while (iNumAveragedFrames < 50)
 	{
 		cap >> frameCam;
 		if (frameCam.empty())
@@ -98,7 +114,7 @@ int main()
 		else
 			cout << "!!!MAX_OBJ_OFFSET!!!" << object_offset << " norm = " << norm(object_offset) << endl;
 
-		Mat img_obj_b = imgRef(roiRefTemplate).clone();
+		Mat img_obj_b = imgRef(roiRefTemplate).clone();	// extracted image without stabilization
 		//imgAvgB += img_obj_b;
 		normalize(img_obj_b, img_obj_b, 0, 255, NORM_MINMAX);
 		img_obj_b.convertTo(img_obj_b, CV_8U);
